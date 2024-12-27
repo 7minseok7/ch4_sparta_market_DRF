3432d117932570feec3b4e16f7e5df36ca64146f
7minseok7 <mansang99@gmail.com>
2024-12-27 17:08:25 +0900

    함수형 뷰에서 클래스형 뷰로 전환

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = "products"
 urlpatterns = [
-    path("", views.product_list, name="product_list"),
-    path("<int:productId>/", views.product_detail, name="product_detail"),
+    # as_view() 메서드를 사용하여 클래스 내 호출 가능한 함수로 변환할 수 있다.
+    path("", views.ProductListAPIView.as_view(), name="product_list"),
+    path("<int:productId>/", views.ProductDetailAPIView.as_view(), name="product_detail"),
 ]
--- products/views.py
+++ products/views.py
@@ -3,23 +3,21 @@ from .models import Product
 from django.shortcuts import get_object_or_404
 
 # Django Rest Framework 관련
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import ProductSerializer
 from rest_framework import status  # 조금 더 읽기 쉬운 status code 구성을 제공함.
 
-# 아무것도 적지 않으면 `GET` 만 허용, 이외의 요청일 경우 `405 Method Not Allowed`
-# DRF의 함수형 뷰의 경우 필수적으로 작성이 필요함.
-# 여기서는 GET, POST로 메서드 제한
-@api_view(["GET", "POST"])
-def product_list(request):
-    # Serializer를 마치 데이터가 바인딩된 폼처럼 생각하면 된다.
-    if request.method == "GET":  # GET일 때에 대한 처리
+# 특정 Http Method에 대한 처리를 함수로 분리하기 위해 클래스형 뷰로 변환
+class ProductListAPIView(APIView):
+    def get(self, request):  # GET 메서드에 대한 처리
+
+        # Serializer를 마치 데이터가 바인딩된 폼처럼 생각하면 된다.
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
-    
-    elif request.method == "POST":  # POST일 때에 대한 처리
+
+    def post(self, request):  # POST 메서드에 대한 처리
         serializer = ProductSerializer(data=request.data)
 
         # is_valid() 메서드의 raise_exception=True 옵션을 주면,
@@ -29,22 +27,24 @@ def product_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-# GET, DELETE로 메서드 제한
-@api_view(["GET", "PUT", "DELETE"])
-def product_detail(request, productId):
-    product = get_object_or_404(Product, id=productId)
+class ProductDetailAPIView(APIView):
+    def get_object(self, productId):
+        return get_object_or_404(Product, id=productId)
 
-    if request.method == "GET":
+    def get(self, request, productId):
+        product = self.get_object(productId)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
-    
-    elif request.method == "PUT":
+        
+    def put(self, request, productId):
+        product = self.get_object(productId)
         serializer = ProductSerializer(product, data=request.data, partial=True)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data)
-        
-    elif request.method == "DELETE":
+            
+    def delete(self, request, productId):
+        product = self.get_object(productId)
         product.delete()
         data = {"delete": f"Product({productId}) is deleted."}
         return Response(data, status=status.HTTP_200_OK)
