13817c9fc1fb6887bb78948b1de61e9514b3894e
7minseok7 <mansang99@gmail.com>
2024-12-27 16:52:52 +0900

    기본 CRUD 구현

--- products/views.py
+++ products/views.py
@@ -1,42 +1,50 @@
-from django.shortcuts import render
-from django.core import serializers
-from django.http import JsonResponse, HttpResponse
 from .models import Product
+
+from django.shortcuts import get_object_or_404
 
 # Django Rest Framework 관련
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import ProductSerializer
+from rest_framework import status  # 조금 더 읽기 쉬운 status code 구성을 제공함.
 
-def product_list_html(request):
-    products = Product.objects.all()
-    context = {"products": products}
-    return render(request, "products/products.html", context)
+# 아무것도 적지 않으면 `GET` 만 허용, 이외의 요청일 경우 `405 Method Not Allowed`
+# DRF의 함수형 뷰의 경우 필수적으로 작성이 필요함.
+# 여기서는 GET, POST로 메서드 제한
+@api_view(["GET", "POST"])
+def product_list(request):
+    # Serializer를 마치 데이터가 바인딩된 폼처럼 생각하면 된다.
+    if request.method == "GET":  # GET일 때에 대한 처리
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == "POST":  # POST일 때에 대한 처리
+        serializer = ProductSerializer(data=request.data)
 
+        # is_valid() 메서드의 raise_exception=True 옵션을 주면,
+        # 조건 불만족 시 serializers.ValidationError를 발생하게 되고
+        # 이렇게 되면 DRF 내부의 예외처리 로직에 의해 처리되어 400(Bad Request) 응답을 반환
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-def json_01(request):
-    products = Product.objects.all()
-    json_res = []
+# GET, DELETE로 메서드 제한
+@api_view(["GET", "PUT", "DELETE"])
+def product_detail(request, productId):
+    product = get_object_or_404(Product, id=productId)
 
-    for product in products:
-        json_res.append(
-            {
-                "title": product.title,
-                "content": product.content,
-                "created_at": product.created_at,
-                "updated_at": product.updated_at,
-            }
-        )
-
-    return JsonResponse(json_res, safe=False)
-
-def json_02(request):
-    products = Product.objects.all()
-    res_data = serializers.serialize("json", products)
-    return HttpResponse(res_data, content_type="application/json")
-
-@api_view(["GET"])
-def json_drf(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    
+    elif request.method == "PUT":
+        serializer = ProductSerializer(product, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        
+    elif request.method == "DELETE":
+        product.delete()
+        data = {"delete": f"Product({productId}) is deleted."}
+        return Response(data, status=status.HTTP_200_OK)
